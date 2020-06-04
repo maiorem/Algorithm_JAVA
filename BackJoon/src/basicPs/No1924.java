c165e8ee5a8bafe4e194fb73ab25867f379b7825
Hong Seyoung <maiorem00@gmail.com>
2020-06-04 17:26:37 +0900

    backjoon

---
+++ BackJoon/src/basicPs/No1924.java
@@ -0,0 +1,18 @@
+package basicPs;
+
+import java.util.Scanner;
+
+public class No1924 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int x=sc.nextInt();
+		int y=sc.nextInt();
+		
+
+
+	}
+
+}
