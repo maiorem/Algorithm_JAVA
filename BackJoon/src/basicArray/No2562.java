7ad425784f65d3b1136c14fb02065f12156b1268
maiorem <maiorem00@gmail.com>
2020-10-06 21:09:36 +0900

    기본 배열

--- BackJoon/src/basicArray/No2562.java
+++ BackJoon/src/basicArray/No2562.java
@@ -1,10 +1,38 @@
 package basicArray;
 
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
 public class No2562 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+
+		int N=Integer.parseInt(sc.nextLine());
+
+		int[] arr=new int[N];
+		StringTokenizer st = new StringTokenizer(sc.nextLine(), " ");
+
+		for(int i=0; i<N; i++) {
+			arr[i]=Integer.parseInt(st.nextToken());
+		}
+
+		int min=arr[0];
+		for(int i=0; i<arr.length; i++) {
+			if (min > arr[i]) {
+				min=arr[i];
+			}
+		}
+		int max=arr[0];
+		for(int j=0; j<arr.length; j++) {
+			if(max < arr[j]) {
+				max=arr[j];
+			}
+		}
+
+		System.out.println(min+" "+max);
 
 	}
-
 }
+
+
