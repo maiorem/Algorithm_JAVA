587b63be52602b637cf094c9a21a5b52c7574718
maiorem <maiorem00@gmail.com>
2020-10-07 20:48:29 +0900

    Update No2562.java

--- BackJoon/src/basicArray/No2562.java
+++ BackJoon/src/basicArray/No2562.java
@@ -1,36 +1,31 @@
 package basicArray;
 
 import java.util.Scanner;
-import java.util.StringTokenizer;
 
 public class No2562 {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-
-		int N=Integer.parseInt(sc.nextLine());
-
-		int[] arr=new int[N];
-		StringTokenizer st = new StringTokenizer(sc.nextLine(), " ");
-
-		for(int i=0; i<N; i++) {
-			arr[i]=Integer.parseInt(st.nextToken());
+		int count=1;
+		int[] arr= new int[9];
+		
+		for (int i=0; i<arr.length; i++) {
+			arr[i]=sc.nextInt();
+			
 		}
-
-		int min=arr[0];
-		for(int i=0; i<arr.length; i++) {
-			if (min > arr[i]) {
-				min=arr[i];
-			}
-		}
+		
 		int max=arr[0];
 		for(int j=0; j<arr.length; j++) {
+			
+			
 			if(max < arr[j]) {
 				max=arr[j];
+				count=j+1;
 			}
 		}
-
-		System.out.println(min+" "+max);
+		
+		System.out.println(max);
+		System.out.println(count);
 
 	}
 }
