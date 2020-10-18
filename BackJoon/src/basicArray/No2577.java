658c7dfd782b5de74c0a8df5b572b1a96b665d19
maiorem <maiorem00@gmail.com>
2020-10-19 07:39:16 +0900

    No 2577 배열문제

--- BackJoon/src/basicArray/No2577.java
+++ BackJoon/src/basicArray/No2577.java
@@ -1,9 +1,29 @@
 package basicArray;
 
+import java.util.Scanner;
+
 public class No2577 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Scanner sc=new Scanner(System.in);
+        
+		int A=Integer.parseInt(sc.nextLine());
+		int B=Integer.parseInt(sc.nextLine());
+		int C=Integer.parseInt(sc.nextLine());
+		
+		int result=A*B*C;
+		int[] count=new int[10];
+		
+		while (result>0) {
+			count[result%10]=count[result%10]+1;
+			result=result/10;
+		}
+
+		
+		for (int i=0; i<count.length; i++) {
+			System.out.println(count[i]);
+		}
 
 	}
 
