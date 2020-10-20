5ea50f79daab345a97c4c89f4d1041d0cb674052
maiorem <maiorem00@gmail.com>
2020-10-20 17:49:33 +0900

    배열 나머지 문제

--- BackJoon/src/basicArray/No3052.java
+++ BackJoon/src/basicArray/No3052.java
@@ -1,9 +1,35 @@
 package basicArray;
 
+import java.util.Scanner;
+
 public class No3052 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner(System.in);
+		int[] arr=new int[10];
+		int count=0;
+
+		
+		for (int i=0; i<10; i++) {
+			arr[i]=Integer.parseInt(sc.nextLine())%42;
+		}
+		
+		
+		for (int i=0; i<10; i++) {
+			int duple=0;
+			for (int j=i+1; j<10; j++) {
+				if (arr[i]==arr[j]) {
+					duple++;
+				}
+			}
+			if (duple==0) {
+				count++;
+			}
+		}
+		
+		System.out.println(count);
+		
 
 	}
 
